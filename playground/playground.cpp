c40fb75b2a495c1c075ad159554f39b9d2e01264
Your Name <15521188045@163.com>
2016-09-11 16:11:23 +0800

    2016.9.11

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -12,7 +12,7 @@
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 // Window dimensions
-const GLuint WIDTH = 800, HEIGHT = 600;
+const GLuint WIDTH = 800, HEIGHT = 600; 
 
 // The MAIN function, from here we start the application and run the game loop
 int main()
